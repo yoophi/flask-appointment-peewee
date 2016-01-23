b43c3aac15d35878d61bf826690db9d1e26a26d0
Pyunghyuk Yoo <yoophi@gmail.com>
2016-01-23 23:15:48 +0900

    model relation fix. user password hash.

--- sched/models.py
+++ sched/models.py
@@ -1,16 +1,8 @@
 from datetime import datetime
-from peewee import BaseModel, IntegerField, DateTimeField, CharField, BooleanField, ForeignKeyField, TextField, Model
-# from sqlalchemy import Column, ForeignKey
-# from sqlalchemy import Boolean, DateTime, Integer, String, Text
-# from sqlalchemy.orm import relationship, synonym
-# from sqlalchemy.ext.declarative import declarative_base
-# from werkzeug import check_password_hash, generate_password_hash
+from peewee import BaseModel, DateTimeField, CharField, BooleanField, ForeignKeyField, TextField, Model
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from sched.app import database
 
-
-# Base = declarative_base()
 
 class BaseModel(Model):
     class Meta:
@@ -18,29 +10,18 @@ class BaseModel(Model):
 
 
 class User(BaseModel):
-    # __tablename__ = 'user'
-
-    id = IntegerField(primary_key=True)
     created = DateTimeField(default=datetime.now)
     modified = DateTimeField(default=datetime.now)
 
     name = CharField(max_length=200)  # Column('name', String(200))
     email = CharField(max_length=100, unique=True, null=False)  # Column(String(100), unique=True, nullable=False)
     active = BooleanField(default=True)  # Column(Boolean, default=True)
-
-    # _password = Column('password', String(100))
     password = CharField(max_length=100)
 
-    # def _get_password(self):
-    #     return self._password
-    #
-    # def _set_password(self, password):
-    #     if password:
-    #         password = password.strip()
-    #     self._password = generate_password_hash(password)
-    #
-    # password_descriptor = property(_get_password, _set_password)
-    # password = synonym('_password', descriptor=password_descriptor)
+    def set_password(self, password):
+        if password:
+            password = password.strip()
+        self.password = generate_password_hash(password)
 
     def save(self, *args, **kwargs):
         self.modified = datetime.now()
@@ -55,11 +36,9 @@ class User(BaseModel):
             return False
         return check_password_hash(self.password, password)
 
-
     @classmethod
     def authenticate(cls, email, password):
         email = email.strip().lower()
-        # user = query(cls).filter(cls.email == email).first()
         user = User.select().where(cls.email == email).get()
         if user is None:
             return None, False
@@ -85,15 +64,9 @@ class User(BaseModel):
 
 class Appointment(BaseModel):
     """An appointment on the calendar."""
-
-    # __tablename__ = 'appointment'
-
-    id = IntegerField(primary_key=True)  # Column(Integer, primary_key=True)
     created = DateTimeField(default=datetime.now)  # Column(DateTime, default=datetime.now)
     modified = DateTimeField(default=datetime.now)  # Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
-    # user_id = IntegerField()# Column(Integer, ForeignKey('user.id'), nullable=False)
-    # user = # relationship(User, lazy='joined', join_depth=1, viewonly=True)
     user = ForeignKeyField(User)
 
     title = CharField()  # Column(String(255))
@@ -121,25 +94,24 @@ if __name__ == '__main__':
 
     database.connect()
     database.create_tables([User, Appointment])
+    now = datetime.now()
 
     # Add a sample user.
     user = User(name='Pyunghyuk Yoo',
                 email='yoophi@gmail.com',
-                password='secret')
-    # print 'user.save()', user.save()
-    # print 'user.id', user.id
-    # print User.select(User.email == 'yoophi@gmail.com').first()
-    #
-    # print User.select().first()
+                )
+    user.set_password('secret')
+    user.save()
 
-    now = datetime.now()
     Appointment(
         user=user,
         title='Important Meeting',
         start=now + timedelta(days=3),
         end=now + timedelta(days=3, seconds=3600),
         allday=False,
-        location='The Office').save()
+        location='The Office',
+        description='...',
+    ).save()
 
     Appointment(
         user=user,
@@ -147,7 +119,9 @@ if __name__ == '__main__':
         start=now - timedelta(days=3, seconds=3600),
         end=now - timedelta(days=3),
         allday=False,
-        location='The Office').save()
+        location='The Office',
+        description='...',
+    ).save()
 
     Appointment(
         user=user,
@@ -155,11 +129,16 @@ if __name__ == '__main__':
         start=now + timedelta(days=4),
         end=now + timedelta(days=4, seconds=3600),
         allday=False,
-        location='The Office').save()
+        location='The Office',
+        description='...',
+    ).save()
 
     Appointment(
         user=user,
         title='Day Off',
         start=now + timedelta(days=5),
         end=now + timedelta(days=5),
-        allday=True).save()
+        allday=True,
+        location='The Office',
+        description='...',
+    ).save()
